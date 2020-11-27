fbc5350f282ce166ee8138a874033b7e9e61f197
pavan1929 <pavan.koilada@gmail.com>
2020-11-27 14:32:05 +0530

    reset logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -58,6 +58,9 @@ export class AppComponent {
   };
   filterReset(){
     this.filterData = {};
+    this.selectedYear=null;
+    this.selectedLaunch=null;
+    this.selectedLand=null;
     this.fetchData();
   };
 }
